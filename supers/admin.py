78c4878be4b78648fe06f6ca8375b9c50a1d9fcb
davidbritton1 <102608670+davidbritton1@users.noreply.github.com>
2022-04-18 09:22:55 -0700

    Minor Updates to views.py & serializers.py, also Configured both admin.py files along with fixing urls.py

--- supers/admin.py
+++ supers/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import HeroesVillians
 
 # Register your models here.
+admin.site.register(HeroesVillians)
--- supers/serializers.py
+++ supers/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
+
+from super_types.models import SuperType
 from .models import HeroesVillians
 
-class HeroesVillianSerializer(serializers.ModelSerializer):
+class HeroesVillianSerializer(serializers.ModelSerializer, SuperType):
     class Meta:
         model = HeroesVillians
-        fields = ['name','alter_ego', 'primary_ability', 'secondary_ability','catchphrase', 'pk']
+        fields = ['name','alter_ego', 'primary_ability', 'secondary_ability','catchphrase', SuperType]
+        
